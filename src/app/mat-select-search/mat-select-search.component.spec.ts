64f7d32b8e408c3d1d32314eeeb992af87a72a18
Alexander <valverin95@gmail.com>
2023-06-27 22:08:16 +0300

    [test] Inputs and Outputs

--- src/app/mat-select-search/mat-select-search.component.spec.ts
+++ src/app/mat-select-search/mat-select-search.component.spec.ts
@@ -41,22 +41,26 @@ describe('MatSelectSearchComponent', () => {
   const debugSelect = () => fixture.debugElement.query(By.css('#select'));
   const debugInput = () => fixture.debugElement.query(By.css('.select-search-input'));
   const debugOptions = () => fixture.debugElement.queryAll(By.css('mat-option'))
+  const debugLabel = () => fixture.debugElement.query(By.css('mat-label'))
+  const debugNotFoundLabel = () => fixture.debugElement.query(By.css('.select-search-not-found'))
+
+  const openSelect = () => {
+    const select = debugSelect().nativeElement as HTMLElement;
+    select.click();
+    fixture.detectChanges();
+  }
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   it('should has a search field', () => {
-    const select = debugSelect().nativeElement as HTMLElement;
-    select.click();
-    fixture.detectChanges();
+    openSelect();
     expect(debugInput().nativeElement).toBeTruthy();
   });
 
   it('should show all options', () => {
-    const select = debugSelect().nativeElement as HTMLElement;
-    select.click();
-    fixture.detectChanges();
+    openSelect();
 
     const optionsLenght = component.items.length;
     expect(debugOptions().length).toBe(optionsLenght);
@@ -67,24 +71,19 @@ describe('MatSelectSearchComponent', () => {
   });
 
   it('should input be focused', async () => {
-    const select = debugSelect().nativeElement as HTMLElement;
-    select.click();
-    fixture.detectChanges();
+    openSelect();
     await fixture.whenStable();
 
     expect(debugInput().nativeElement).toBe(document.activeElement);
   })
 
   it('should filtering', async () => {
-    const select = debugSelect().nativeElement as HTMLElement;
-    select.click();
-    fixture.detectChanges();
+    openSelect();
 
     const input = debugInput();
     const firstItem = component.items[component.items.length - 1];
     input.nativeElement.value = firstItem;
     input.nativeElement.dispatchEvent(new Event('input'));
-    fixture.detectChanges();
     await fixture.whenStable();
 
     const firstOption = debugOptions().find(opt => {
@@ -92,7 +91,60 @@ describe('MatSelectSearchComponent', () => {
     });
 
     expect(firstOption?.componentInstance.value).toBe(firstItem)
+  });
+
+  it('should show not found label according to @input', async () => {
+    openSelect();
+    expect(debugNotFoundLabel()).not.toBeTruthy();
+
+    const input = debugInput();
+    const [item1, item2] = component.items;
+    input.nativeElement.value = item1 + item2;
+    input.nativeElement.dispatchEvent(new Event('input'));
+    await fixture.whenStable();
+    fixture.detectChanges();
+
+    expect(debugNotFoundLabel()).toBeTruthy();
+
+    const label = 'my label'
+    component.notFoundLabel = label;
+    fixture.detectChanges();
+    expect(debugNotFoundLabel().nativeElement.innerText).toContain(label);
   })
+
+  it('should set placeholder from @input', () => {
+    openSelect();
+
+    const placeholder = 'my placeholder';
+    component.placeholder = placeholder;
+    fixture.detectChanges();
+
+    expect(debugInput().nativeElement.placeholder).toBe(placeholder);
+  });
+
+  it('should set label from @input', () => {
+    openSelect();
+
+    const label = 'my label';
+    component.label = label;
+    fixture.detectChanges();
+
+    expect(debugLabel().nativeElement.innerText).toContain(label)
+  });
+
+  it('should emit filterChange @output', async () => {
+    openSelect();
+    spyOn(component, 'filterChange');
+
+    const input = debugInput();
+    const firstItem = component.items[0];
+    input.nativeElement.value = firstItem;
+    input.nativeElement.dispatchEvent(new Event('input'));
+    await fixture.whenStable();
+    fixture.detectChanges();
+
+    expect(component.filterChange).toHaveBeenCalledWith(firstItem);
+  });
 });
 
 
@@ -100,7 +152,13 @@ describe('MatSelectSearchComponent', () => {
   template: `
     <mat-form-field id="field">
       <mat-select id="select">
-        <mat-select-search id="select-search"/>
+        <mat-select-search 
+          id="select-search"
+          [placeholder]="placeholder"
+          [label]="label"
+          [notFoundLabel]="notFoundLabel"
+          (filterChange)="filterChange($event)"
+        />
 
         <mat-option *ngFor="let item of items" [value]="item">
           {{ item }}
@@ -110,5 +168,13 @@ describe('MatSelectSearchComponent', () => {
   `,
 })
 class MatSelectSearchExample {
+  placeholder = 'placeholder';
+  label = 'label';
+  notFoundLabel = 'notFoundLabel';
+  sticky = 'sticky';
   items = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
+
+  filterChange(v: string | null) {
+    console.log('filterChange', v);
+  }
 }
